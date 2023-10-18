4865060397e6792123ba904c8c74a7e043d5ddb9
Yadukrishnan <yaduk.tv@gmail.com>
2023-10-18 21:59:07 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,81 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Input, Button } from 'antd';
+import CryptoJS from 'crypto-js';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [email, setEmail] = useState<any>('');
+  const [generatedVsKey, setgeneratedVsKey] = useState<string>();
+  const [generatedSubKey, setgeneratedSubKey] = useState<string>();
+  const [emailError, setEmailError] = useState<boolean>(false);
+
+  const handleChange = (event: { target: { value: any; }; }) => {
+    const value = event.target.value;
+    setEmail(value);
+  };
+  
+  const validateEmail = () => {
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if( emailRegex.test(email) == true) {
+      setgeneratedVsKey(keygen_vscode(email));
+      setgeneratedSubKey(keygen_sublime(email));
+      setEmailError(false);
+    } else {
+      setEmailError(true);
+      setgeneratedVsKey('');
+      setgeneratedSubKey('');
+    }
+  };
+
+  function keygen_vscode(email: string) {
+    const fillerString = 'fd330f6f-3f41-421d-9fe5-de742d0c54c0';
+    const hash = CryptoJS.MD5(`${fillerString}${email}`).toString().slice(0, 25);
+    return(keygenInsertSeparator(hash))
+  };
+
+  function keygen_sublime(email: string) {
+    const hash = CryptoJS.MD5(email).toString().slice(0, 25);
+    return(keygenInsertSeparator(hash));
+  }
+
+  function keygenInsertSeparator(generater_key: string) {
+    let counter = 1;
+    let keyFinal = '';
+  
+    for (let i = 0; i < generater_key.length; i++) {
+      const letter = generater_key[i];
+  
+      if (counter % 5 === 0 && counter !== 25) {
+        keyFinal += letter + '-';
+      } else {
+        keyFinal += letter;
+      }
+  
+      counter++;
+    }
+    return(keyFinal);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h2>Monokai Pro Key Generator</h2>
+      <div className="email">
+        <p className='email-text'>Enter your email address: </p>
+        <Input onChange={handleChange} className='email-input'/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <Button className='confirm-button' type='primary' onClick={validateEmail}>Confirm</Button>
+      <div className="key-output">
+        {generatedVsKey && (
+          <p>VS Code key: <span className='key'>{generatedVsKey}</span></p>
+        )}
+        {generatedVsKey && (
+          <p>Sublime Editor key: <span className='key'>{generatedSubKey}</span></p>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {emailError && (
+        <p style={{'fontWeight':500}}>Enter a valid Email!!!</p>
+      )}
     </>
   )
 }
